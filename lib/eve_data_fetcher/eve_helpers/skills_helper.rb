0014924cb5893771e7c1698ed62825ee959bddc9
Karl Turner <karl.turner5@gmail.com>
2014-05-01 09:25:00 +0100

    removed table insie table as looked terrible

--- lib/eve_data_fetcher/eve_helpers/skills_helper.rb
+++ lib/eve_data_fetcher/eve_helpers/skills_helper.rb
@@ -2,18 +2,19 @@ module EveHelpers
   # Help weed out the skills in nice format
   module SkillsHelper
     def skill_response(skill_response)
+      @skill_map = skill_mapping(skill_response)
       groups = []
-      known_groups(skill_response).each do |key|
+      skill_known_groups(skill_response).each do |key|
         skills = []
         default_rows(skill_response).each do |row|
           skills = compile_skills(row, skills) if skill_group_name(row) == key
         end
-        groups << group_hash(key, skills.compact)
+        groups << skill_group_hash(key, skills.compact)
       end
       groups
     end
 
-    def group_hash(key, skills)
+    def skill_group_hash(key, skills)
       {
         group: key,
         skills: skills
@@ -40,15 +41,15 @@ module EveHelpers
     end
 
     def required_skills(skill)
-      required_skills = []
-      skill_bonus_collection = []
+      req_skills = []
+      sb_collection = []
       skill['rowset'].each do |rq|
-        required_skills << rq['row'] if required_skill(rq['name'])
-        skill_bonus_collection << rq['row'] if bonus_collection(rq['name'])
+        req_skills << formatted_skills(rq['row']) if required_skill(rq['name'])
+        sb_collection << rq['row'] if skill_bonus_collection(rq['name'])
       end
       {
-        required_skills: required_skills.compact,
-        skill_bonus_collection: skill_bonus_collection.compact
+        required_skills: req_skills.compact,
+        skill_bonus_collection: sb_collection.compact
       }
     end
 
@@ -60,7 +61,7 @@ module EveHelpers
       name == 'requiredSkills'
     end
 
-    def bonus_collection(name)
+    def skill_bonus_collection(name)
       name == 'skillBonusCollection'
     end
 
@@ -68,12 +69,52 @@ module EveHelpers
       row['groupName']
     end
 
-    def known_groups(response)
+    def skill_known_groups(response)
       default_rows(response).map { |a| a['groupName'] }.uniq
+    end
+
+    def skill_mapping(response)
+      skill_map = {}
+      default_rows(response).map do |data|
+        if skill_skills(data).kind_of?(Hash)
+          skill_map.merge!(skill_data_hash(data))
+        end
+        skill_skills(data).each do |sk|
+          (skill_map[sk['typeID']] = sk['typeName']) if sk.kind_of?(Hash)
+        end
+      end
+      skill_map
     end
 
     def skill_skills(data)
       data['rowset']['row']
+    end
+
+    def formatted_skills(required_skills)
+      return unless required_skills
+      if required_skills.kind_of?(Hash)
+        required_skills.merge!(compile_skill_hash(required_skills))
+      end
+      required_skills.each do |skill|
+        if skill.kind_of?(Hash)
+          skill.merge!('name' => @skill_map[skill['typeID']])
+        end
+      end
+    end
+
+    def compile_skill_hash(required_skills)
+      return unless required_skills
+      return if @skill_map[required_skills['typeID']].nil?
+      {
+        'name' => @skill_map[required_skills['typeID']]
+      }
+    end
+
+    def skill_data_hash(data)
+      return unless data
+      {
+        skill_skills(data)['typeID'] => skill_skills(data)['typeName']
+      }
     end
   end
 end
