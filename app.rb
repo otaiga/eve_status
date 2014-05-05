0f3ed6e19d611050235d02b053e70e00d4c6b20d
Karl Turner <karl.turner5@gmail.com>
2014-05-05 13:53:36 +0100

    custom cache per page

--- app.rb
+++ app.rb
@@ -7,15 +7,12 @@ end
 
 helpers Sinatra::ViewHelpers
 
-before '/*' do
-  cache_control :public, max_age: 86400
-end
-
 get '/' do
   erb :index
 end
 
 get '/alliance_info' do
+  cache_control :public, max_age: 86400
   @data = EveDataFetcher::Eve.alliance_info
   @title = 'Alliance info'
   @path = 'alliance_info.json'
@@ -23,24 +20,28 @@ get '/alliance_info' do
 end
 
 get '/conquerable_stations' do
+  cache_control :public, max_age: 86400
   @data = EveDataFetcher::Eve.conquerable_stations
   @title = 'Conquerable Stations'
   erb :eve_data
 end
 
 get '/factional_warfare_stats' do
+  cache_control :public, max_age: 86400
   @data = EveDataFetcher::Eve.factional_warfare_stats
   @title = 'Factional Warfare Stats'
   erb :eve_data
 end
 
 get '/reference_types' do
+  cache_control :public, max_age: 86400
   @data = EveDataFetcher::Eve.reference_types
   @title = 'Reference Types'
   erb :eve_data
 end
 
 get '/skill_tree' do
+  cache_control :public, max_age: 3600
   @data = EveDataFetcher::Eve.skill_tree
   @title = 'Skill Tree'
   erb :skill_data
