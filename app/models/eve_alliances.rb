efdbc4c5c5d3b7b54cdb4d77f9e6fc4a94af4be9
Karl Turner <karl.turner5@gmail.com>
2014-05-30 10:11:09 +0100

    updated schema and included the ingestion task for alliance, now for the view.

--- app/models/eve_alliances.rb
+++
@@ -1,6 +0,0 @@
-# Patch for the alliances module
-module EveAlliances
-  def self.table_name_prefix
-    'eve_alliances_'
-  end
-end
--- spec/models/eve_skill_spec.rb
+++
@@ -1,5 +0,0 @@
-require 'spec_helper'
-
-describe EveSkill do
-  pending "add some examples to (or delete) #{__FILE__}"
-end
